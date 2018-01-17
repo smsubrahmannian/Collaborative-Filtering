642cc62ce018e5cffe8131e27739911b8e84286b
EC2 Default User <ec2-user@ip-172-31-30-248.us-west-2.compute.internal>
2018-01-17 21:28:07 +0000

    preprocessing file adjusted

--- codes/preprocessing.py
+++ codes/preprocessing.py
@@ -31,16 +31,17 @@ bizMap = bizMap.withColumn("biz_ix", monotonically_increasing_id())
 
 ## Join Dataframe to the review table
 
-df_review = df_review.select('user_id','business_id','stars')
-df_review = df_review.join(userMap,on='user_id',how='left_outer').join(bizMap,on='business_id',how='left_outer')
-ALS_baseline_df = df_review.select('user_ix','biz_ix','stars')
-
+df_review_als = df_review.select('user_id','business_id','stars')
+df_review_als = df_review_als.join(userMap,on='user_id',how='left_outer').join(bizMap,on='business_id',how='left_outer')
+ALS_baseline_df = df_review_als.select('user_ix','biz_ix','stars')
+df_tip_df = df_tip.join(userMap,on='user_id',how='left_outer').join(bizMap,on='business_id',how='left_outer')
 ## drop and Save table
 
 userMap.write.parquet("s3://dknsyelp/userMap.parquet", mode='overwrite')
 bizMap.write.parquet("s3://dknsyelp/bizMap.parquet", mode='overwrite')
 ALS_baseline_df.write.parquet("s3://dknsyelp/ALS_baseline.parquet", mode='overwrite')
 df_review.write.parquet("s3://dknsyelp/review.parquet", mode='overwrite')
+df_tip_df.write.parquet("s3://dknsyelp/tip.parquet", mode='overwrite')
 
 print 'Successful'
 
