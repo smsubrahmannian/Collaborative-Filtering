39ce235033bc0003e3b4faddb243df6f5a9f2357
nsinha2 <nsinha2@dons.usfca.edu>
2018-01-18 14:56:39 -0800

    updated

--- codes/t-sne.py
+++ codes/t-sne.py
@@ -17,7 +17,7 @@ import bokeh.plotting as bp
 from bokeh.models import HoverTool, BoxSelectTool
 from bokeh.plotting import figure, show, output_notebook
 from bokeh.plotting import figure, output_file, save
-
+from bokeh.plotting import figure, output_file, save
 
 sqlContext = SQLContext(sc)
 
@@ -47,33 +47,44 @@ als = ALS(maxIter=50, regParam=1, userCol="user_ix",nonnegative=True
 
 model = als.fit(ratings)
 
+#Embeddings
+
 a = model.itemFactors
 b= a.sort("id")
 b.show()
 
-
+#Creating a dense matrix from embedding for businesses
 values = (b.rdd.map(lambda x: (x.id, x.features)).sortByKey().flatMap(lambda (x, y): y).collect())
 
 nrow = len(b.rdd.map(lambda x: x.features).first())
 ncol = b.count()
 
-
 dm = DenseMatrix(nrow, ncol, values)
 dm.toArray().shape
 z=dm.toArray().transpose()
 
+#t-sne
 
 tsne = TSNE(n_components=2)
 X_tsne = tsne.fit_transform(z)
+
+# creating data frame with t-sne results and business_id
+e = sqlContext.createDataFrame(pd.DataFrame(X_tsne))
+e_df = e.toPandas()
+j=b.select("id")
+j_df =j.toPandas()
+result = pd.concat([e_df, j_df], axis=1,ignore_index=True)
+result = pd.DataFrame(result)
 
 output_notebook()
 plot = bp.figure(plot_width=700, plot_height=600, title="Clustering of the restaurants",
     tools="pan,wheel_zoom,box_zoom,reset,hover,previewsave",
     x_axis_type=None, y_axis_type=None, min_border=1)
+## plotting using bookeh
 
-
-
-plot.scatter(x=X_tsne[:,0] , y=X_tsne[:,1])
+plot.scatter(x='a' , y='b', source = result)
 hover = plot.select(dict(type=HoverTool))
-#hover.tooltips={"description":"@a.select('id')" }
+hover.tooltips={"description":"@c"}
+output_file("output_file_name.html")
 show(plot)
+
